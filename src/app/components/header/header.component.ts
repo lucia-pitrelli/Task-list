9d85506c5896b44202b9cc35bf9b26027974863b
lucia-pitrelli <pitrelliblucia@gmail.com>
2023-03-05 02:24:41 -0300

    generate new component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  title: string = 'Task List'; //una manera de hacer binding del ts al component
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
